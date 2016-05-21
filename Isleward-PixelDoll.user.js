42a0a294a774b3cf84960dedbe23ea2bab8390db
Martin Rocek <martin@oculum.ink>
2016-05-21 19:13:29 +0200

    url update

--- Isleward-PixelDoll.user.js
+++ Isleward-PixelDoll.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Isleward - Pixeldoll UI
 // @namespace    Isleward.Addon
-// @version      1.1
+// @version      1.2
 // @description  Add paper-doll ui to inventory
 // @author       Silence.sys
 // @match        isleward-test.herokuapp.com/*
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
-// @resource     pixelDoll https://cdn.rawgit.com/silencesys/Pixel-doll-ui/master/pixel-doll.css?v=2.9
+// @resource     pixelDoll https://cdn.rawgit.com/Isleward-Addon-Dev-Group/Pixel-doll-ui/master/pixel-doll.css?v=2.9
 // ==/UserScript==
 
 (function() {
@@ -15,6 +15,6 @@
     GM_addStyle(css);
     var scriptElement = document.createElement( "script" );
     scriptElement.type = "text/javascript";
-    scriptElement.src = "https://cdn.rawgit.com/silencesys/Pixel-doll-ui/master/pixel-doll.js?v=1.1";
+    scriptElement.src = "https://cdn.rawgit.com/Isleward-Addon-Dev-Group/Pixel-doll-ui/master/pixel-doll.js?v=1.1";
     document.body.appendChild( scriptElement );
 })();
