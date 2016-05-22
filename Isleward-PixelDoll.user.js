befb7c6a24bb4fa3b5b2e044d023e9d368759fd3
Martin Rocek <martin@oculum.ink>
2016-05-22 12:31:03 +0200

    raw git fix

--- Isleward-PixelDoll.user.js
+++ Isleward-PixelDoll.user.js
@@ -7,7 +7,7 @@
 // @match        isleward-test.herokuapp.com/*
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
-// @resource     pixelDoll https://cdn.rawgit.com/Isleward-Addon-Dev-Group/Pixel-doll-ui/master/pixel-doll.css?v=3.0
+// @resource     pixelDoll https://cdn.rawgit.com/Isleward-Addon-Dev-Group/Pixel-doll-ui/641e20150be0bf5dc43d1a4594339443c0c499b9/pixel-doll.css?v=3.0
 // ==/UserScript==
 
 (function() {
@@ -15,6 +15,6 @@
     GM_addStyle(css);
     var scriptElement = document.createElement( "script" );
     scriptElement.type = "text/javascript";
-    scriptElement.src = "https://cdn.rawgit.com/Isleward-Addon-Dev-Group/Pixel-doll-ui/master/pixel-doll.js?v=1.5";
+    scriptElement.src = "https://cdn.rawgit.com/Isleward-Addon-Dev-Group/Pixel-doll-ui/641e20150be0bf5dc43d1a4594339443c0c499b9/pixel-doll.js?v=1.5";
     document.body.appendChild( scriptElement );
 })();
